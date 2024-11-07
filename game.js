ed35a6fef02cd35769fcb2f433913d97b9f0b8bb
yash-gautam9953 <yashgautam9953@gmail.com>
2024-11-07 21:01:41 +0530

    Update game.js

--- game.js
+++ game.js
@@ -3,6 +3,7 @@ let compScore=0;
 let choice=document.querySelectorAll(".choice");
 let msg=document.querySelector("#message");
 let uscore=document.querySelector("#userScore");
+console.log(uscore)
 let cscore=document.querySelector("#compScore");
 
 const compChoice = () =>{
@@ -13,45 +14,46 @@ const compChoice = () =>{
 const draw = () =>{
     console.log("match is draw");
     msg.innerText="Match is draw ! play Again";
+    msg.style.backgroundColor="black";
+    msg.style.color="white";
 }
 
-const result =(userWin,yash,harsh) =>{
-    if(userWin){
+const result =(UserWin,yash,harsh) =>{
+    if(UserWin){
         userScore++;
-uscore.innerText=userScore;
+        uscore.innerText=userScore;
         console.log("You win");
         msg.innerText=`You Win! your ${yash} beats ${harsh}`;
    msg.style.backgroundColor="greenyellow";
     }else{
         compScore++;
         cscore.innerText=compScore;
-console.log("You lose");
-msg.innerText=`You Lose!${harsh} beats your ${yash}`;
-msg.style.backgroundColor="red";
+        console.log("You lose");
+        msg.innerText=`YouLose!${harsh} beats your ${yash}`;
+        msg.style.backgroundColor="red";
     }
 
 }
 
 const playGame = (yash) =>{
-    console.log("User choice is =",yash);
+    console.log("user choice is =",yash);
     let harsh=compChoice();
     console.log("computer choice =",harsh);
-    let userWin =true;
+    let UserWin=true;
     if(yash===harsh){
         draw();
-    }else{
-        if(yash==="rock"){
-      userWin = harsh==="paper" ? false :true;
-        }else if(yash==="paper"){
-           userWin=harsh==="rock" ? true :false;
-        }else if(yash==="scissor"){
-userWin=harsh==="rock"?false :true;
-        }
-        console.log(userWin );
-        result(userWin,yash,harsh);
     }
-
+    else if((yash==="rock" && harsh==="paper") || (yash==="scissor" && harsh==="rock") || (yash==="paper" && harsh==="scissor")){
+       UserWin=false;
+       result(UserWin,yash,harsh);
+    }else{
+        result(UserWin,yash,harsh);
+    }
+    
+   
+    
 } 
+
 choice.forEach((val) =>{
 val.addEventListener("click", () => {
     const yash=val.getAttribute("id");
